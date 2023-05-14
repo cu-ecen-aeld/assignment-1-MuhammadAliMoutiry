379e888554a1a56e803abc5e1f1ad5bdf4249966
Muhammad Ali Moutiry <muhammadmoutiry@gmail.com>
2023-05-14 11:27:07 +0300

    Add finder.sh and writer.sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+writefile=$1
+writestr=$2
+
+if [ $# != 2 ]
+   then echo "Expected args are 2 but $# passed"
+   exit 1
+else
+   if [ ! -d $writefile ]; then
+     mkdir -p "$(dirname "$writefile")" && touch "$writefile"
+    else
+        touch "$writefile"
+     fi
+     echo "$writestr" >> $writefile
+fi
+   
