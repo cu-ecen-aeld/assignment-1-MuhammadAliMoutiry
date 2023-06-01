cee8642dce438870aa909fa5987c95e67519f63c
MuhammadAliMoutiry <muhammadmoutiry@gmail.com>
2023-06-02 00:51:04 +0300

    modify return value

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -4,7 +4,7 @@ writestr=$2
 
 if [ $# != 2 ]
    then echo "Expected args are 2 but $# passed"
-   exit 1
+   return 1
 else
    if [ ! -d $writefile ]; then
      mkdir -p "$(dirname "$writefile")" && touch "$writefile"
